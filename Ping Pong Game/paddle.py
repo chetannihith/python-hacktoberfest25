dee6e5719829575d74c87b61849ff28ef16ef6b1
Andrey Blazejuk <andreyblazejuk@hotmail.com>
2025-10-05 06:49:21 -0300

    Add constants to paddle.py

--- Ping Pong Game/paddle.py
+++ Ping Pong Game/paddle.py
@@ -1,22 +1,29 @@
 from turtle import Turtle
 
+PADDLE_COLOR = "black"
+PADDLE_SHAPE = "square"
+PADDLE_WIDTH = 5
+PADDLE_LENGTH = 1
+MOVE_DISTANCE = 20
+UPPER_LIMIT = 250
+LOWER_LIMIT = -250
 
 class Paddle(Turtle):
     
     def __init__(self, position):
         super().__init__()
-        self.shape("square")
-        self.color("black")
-        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.shape(PADDLE_SHAPE)
+        self.color(PADDLE_COLOR)
+        self.shapesize(stretch_wid=PADDLE_WIDTH, stretch_len=PADDLE_LENGTH)
         self.penup()
         self.goto(position)
 
     def go_up(self):
-        if self.ycor() <= 250:
-            new_y = self.ycor() + 20
+        if self.ycor() <= UPPER_LIMIT:
+            new_y = self.ycor() + MOVE_DISTANCE
             self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        if self.ycor() >= -250:
-            new_y = self.ycor() - 20
+        if self.ycor() >= LOWER_LIMIT:
+            new_y = self.ycor() - MOVE_DISTANCE
             self.goto(self.xcor(), new_y)
